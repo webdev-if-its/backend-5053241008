6cba3d5dce31ba0e1010d7a5823fc589a1869d9b
rfdaazht <azadirahta73@gmail.com>
2026-09-15 17:22:09 +0700

    pertemuan-03: level 2 selesai

--- pertemuan-03/main.go
+++ pertemuan-03/main.go
@@ -44,7 +44,12 @@ func TambahTugas(toko *TokoTugas, judul string) (Task, error) {
 }
 
 func LihatTugas(toko *TokoTugas, id int) (Task, error) {
-	panic("belum diimplementasikan")
+	for _, t := range toko.Daftar {
+		if t.ID == id {
+			return t, nil
+		}
+	}
+	return Task{}, nil
 }
 
 func HapusTugas(toko *TokoTugas, id int) error {
